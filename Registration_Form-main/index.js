48c2d1116e59ac26f70a108df5e6dc2f8b176a2b
Mohni7277 <130225599+Mohni7277@users.noreply.github.com>
2024-11-09 20:43:12 +0530

    Update index.js

--- Registration_Form-main/index.js
+++ Registration_Form-main/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
-mongoose.connect('mongodb://localhost:27017/Database')
+mongoose.connect('mongodb://127.0.0.1:27017/Database')
 var db=mongoose.connection
 db.on('error',()=> console.log("Error in Connecting to Database"))
 db.once('open',()=> console.log("Connected to Database"))
